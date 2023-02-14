58ae19ca66dd70680e82e517d7dca743020a3916
Elle Imhoff <Kronic.Deth@gmail.com>
2023-02-14 12:18:21 -0600

    mix new live_view_native_jetpack

---
+++ lib/live_view_native_jetpack.ex
@@ -0,0 +1,18 @@
+defmodule LiveViewNativeJetpack do
+  @moduledoc """
+  Documentation for `LiveViewNativeJetpack`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LiveViewNativeJetpack.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/live_view_native_jetpack_test.exs
@@ -0,0 +1,8 @@
+defmodule LiveViewNativeJetpackTest do
+  use ExUnit.Case
+  doctest LiveViewNativeJetpack
+
+  test "greets the world" do
+    assert LiveViewNativeJetpack.hello() == :world
+  end
+end
